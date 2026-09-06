aa9f94c4898fceef5a23ff179b1e8bc1c018f449
Goh Zyik Xyun <gohzy2021@gmail.com>
2026-09-06 20:45:52 +0800

    Q1 solution

--- NumbersArray.java
+++ NumbersArray.java
@@ -18,8 +18,48 @@ public class NumbersArray {
         System.out.println("Duplicate numbers : " + Arrays.toString(findDuplicates(input)));
         System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
     }
+    
 
     // Write your methods here
+    private static int findMax(Integer[]input) {
+        // 0 1 2 boundary
+        int max = 0;
+        for (int n : input) {
+            if (n > max) max = n;
+        }
+        return max;
+    }
+    private static Integer[] findDuplicates(Integer[] input) {
+        ArrayList<Integer> seen = new ArrayList<>();
+        TreeSet<Integer> duplicates = new TreeSet<>();
+        for (int n: input) {
+            if (!seen.contains(n)) {
+                seen.add(n);
+                continue;
+            }
+            if (seen.contains(n)) {
+                duplicates.add(n);
+            }
+        }
+        return duplicates.toArray(new Integer[0]);
+    }
+    private static Integer[] findUnique(Integer[]input) {
+        ArrayList<Integer> seen = new ArrayList<>();
+        ArrayList<Integer> unique = new ArrayList<>(); 
+        for (int n : input) {
+            if (!seen.contains(n)) {
+                seen.add(n);
+                unique.add(n);
+                continue;
+            }
+            if(seen.contains(n)) {
+                // unique.remove(n);
+                // this removes by index
+                unique.remove(Integer.valueOf(n));
+            }
+        }
+        return unique.toArray(new Integer[0]);
+    }
     
 }
 
